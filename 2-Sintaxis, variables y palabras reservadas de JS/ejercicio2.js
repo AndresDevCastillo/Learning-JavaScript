fb771066e59a6b4c95bd284df9e5a6baeab8c8f5
Andres Castillo <115095588+AndresDevCastillo@users.noreply.github.com>
2023-02-21 15:35:00 -0500

    este es el ejercicio de tipos

---
+++ 2-Sintaxis, variables y palabras reservadas de JS/ejercicio2.js
@@ -0,0 +1,13 @@
+let name_ = "Andres D. Castillo";
+let age = 18;
+let developer = true;
+let birth = new Date("december 03 2004");
+libro = {
+    titulo: "El principe",
+    fecha: 1513,
+    autor: " Nikolas Makiavelo",
+    url: "Nikolas Makiavelo",
+}
+
+lista = [name_, age, developer, birth, libro];
+console.log(lista);
